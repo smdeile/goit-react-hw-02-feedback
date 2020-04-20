db2dd18db91bf2250a52ce0b5580c6790d4630bd
smdeile <smdeile@gmail.com>
2020-04-20 11:02:03 +0300

    Updates

---
+++ precache-manifest.6721d38b478d4a15da07f357158ab3f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97517fd6b352145161689b3302c9ac29",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "875f0bc770293505950c",
+    "url": "/goit-react-hw-02-feedback/static/js/2.1617b5ed.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.1617b5ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb560584a70fbf8309e6",
+    "url": "/goit-react-hw-02-feedback/static/js/main.c2ca89d2.chunk.js"
+  },
+  {
+    "revision": "f59aa7dbc0213e80a103",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.9cb39d68.js"
+  }
+]);
